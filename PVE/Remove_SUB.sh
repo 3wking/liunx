25dad0fbb310b8af437210ff3735f41e7e6d983b
3wking <1094890624@qq.com>
2023-05-15 00:24:31 +0800

    更新 Remove_SUB.sh

--- PVE/Remove_SUB.sh
+++ PVE/Remove_SUB.sh
@@ -6,13 +6,22 @@ RES='\e[0m' #尾
 function Install() {
 	dir="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
 	rows=$(($(grep -n "No valid subscription" $dir |awk -F ':' '{print $1}')-1))
-	text=`sed -n ${tmp}p $dir | grep "Ext.Msg.show({"`
+	text=`sed -n ${rows}p $dir | grep "Ext.Msg.show({"`
 	if [[ $text ]]; then
-		sed -i ''$tmp's/show/noshow/g' $dir
+		echo -e "\r\n${GREEN_COLOR}寻找结果为：${#text}${RES}"
+		echo -e "${GREEN_COLOR}当前内容为：`sed -n ${rows}p $dir`${RES}"
+		sed -i ''$rows's/show/noshow/g' $dir >/dev/null 2>&1 
+		if [ $? -ne 0 ]; then
+			echo -e "${RED_COLOR}内容修改失败.${RES}"
+			echo -e "${RED_COLOR}当前内容为： `sed -n ${rows}p $dir`${RES}\r\n"
+			exit 1
+		fi
 		echo -e "${GREEN_COLOR}内容更改成功...${RES}"
+		echo -e "${GREEN_COLOR}当前内容为：`sed -n ${rows}p $dir`${RES}\r\n"
 	else
+		echo -e "\r\n${RED_COLOR}寻找结果为：${#text}${RES}"
 		echo -e "${RED_COLOR}内容更改失败.${RES}"
-
+		echo -e "${RED_COLOR}当前内容为： `sed -n ${rows}p $dir`${RES}\r\n"
 	fi
 }
 Install
