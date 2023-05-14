77b9ac5decc23438ae9f0bbbd8e92cdc9275576a
3wking <1094890624@qq.com>
2023-05-14 23:33:46 +0800

    创建 Remove_SUB.sh

---
+++ PVE/Remove_SUB.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+RED_COLOR='\e[1;31m' #红色
+GREEN_COLOR='\e[1;32m' #绿色
+RES='\e[0m' #尾
+
+function Install() {
+	dir="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
+	rows=$(($(grep -n "No valid subscription" $dir |awk -F ':' '{print $1}')-1))
+	text=`sed -n ${tmp}p $dir | grep "Ext.Msg.show({"`
+	if [ $text ]; then
+		sed -i ''$tmp's/show/noshow/g' $dir
+		echo -e "${GREEN_COLOR}内容更改成功...${RES}"
+	else
+		echo -e "${RED_COLOR}内容更改失败.${RES}"
+
+	fi
+}
+Install
